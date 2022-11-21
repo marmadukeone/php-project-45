636114e6dfa3c1fe48554764e6ddd2c0c1e29ac8
Александр Сарапулов <marmadukeone@mail.com>
2022-11-21 20:15:16 +0700

    step3

---
+++ src/Cli.php
@@ -0,0 +1,18 @@
+<?php
+namespace BrainGames\Cli;
+require_once 'vendor/autoload.php';
+
+
+use function cli\line;
+use function cli\prompt;
+//use cli;
+
+
+function askName ()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
+
+
